e3aadead69ff6056400efcc4ab7cfdd78644160c
Royler Marichal Carrazana <cluzstudio@gmail.com>
2023-12-27 18:34:33 -0300

    init

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <img src="https://thesaasfactory.dev/_next/image?url=%2Fassets%2Fimg%2Flogo.png&w=128&q=75" alt="" />,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/The-SaaS-Factory/frontend-saas-boilerplate-react-apollo-graphql-TS',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/The-SaaS-Factory/frontend-saas-boilerplate-react-apollo-graphql-TS',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'The SaaS Factory Boilerplate Docs ',
   },
 }
+
+ 
 
 export default config
